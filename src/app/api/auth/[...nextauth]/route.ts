f1169165c97bc7ef96beab36f6d608f2fba96690
shreyas367 <shreyas367@users.noreply.github.com>
2025-07-27 23:44:32 +0530

    sdf

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,7 +4,11 @@ import { authOptions } from "./options";
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
 
+import { getServerSession } from "next-auth/next"; // ✅ for App Router
 
+
+
+import { NextRequest } from "next/server";
 
 
 
