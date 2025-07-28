1e259f653965a77aeb44a8626c7e12b268cb277a
shreyas367 <shreyas367@users.noreply.github.com>
2025-07-28 23:25:09 +0530

    dsss

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,27 +10,30 @@ import { Button } from '@/components/ui/button';
 export default function Navbar() {
   const { data: session } = useSession();
   const pathname = usePathname();
-
   const user: User = session?.user as User;
 
   return (
-    <nav className='p-4 md:p-6 bg-gray-800 text-white flex justify-between items-center'>
+    <nav className='p-4 md:p-6 bg-gray-800 text-white'>
       <div className='container mx-auto flex justify-between items-center'>
-        <a href="#">  FEEDBACK </a>
+        <a href="#">FEEDBACK</a>
 
         {session ? (
           <>
-            <span className="mr-4">
-              Welcome, {user?.username || user?.email}
-            </span>
-            <Button onClick={() => signOut({ callbackUrl: '/sign-in' })}>Logout</Button>
-
+            <div className="flex items-center gap-4">
+              <span>Welcome, {user?.username || user?.email}</span>
+              <Button onClick={() => signOut({ callbackUrl: '/sign-in' })}>Logout</Button>
+            </div>
           </>
         ) : (
           pathname !== '/sign-in' && (
-            <Link href="/sign-in">
-              <Button>Log In</Button>
-            </Link>
+            <div className="flex items-center gap-4">
+              <Link href="/sign-in">
+                <Button variant="default">Log In</Button>
+              </Link>
+              <Link href="/sign-up">
+                <Button >Sign Up</Button>
+              </Link>
+            </div>
           )
         )}
       </div>
